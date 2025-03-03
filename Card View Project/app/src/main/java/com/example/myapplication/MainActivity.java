4bad6536fbdf65198f4a46793a2b572b2d2a75b3
Lucy Trainor <adamt2005@icloud.com>
2025-03-03 11:15:46 +0000

    awesome card view lolo

---
+++ Card View Project/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    /**
+     * Creates an intent to open the card view activity
+     * @param aView an object of the view class
+     */
+    public void showCardOne(View aView)
+    {
+        Intent cardIntent = new Intent(this, CardViewActivity.class);
+        startActivity(cardIntent);
+    }
+
+}
